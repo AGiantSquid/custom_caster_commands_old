d6f5b6a549d5642660516508551148561915422d
AGiantSquid <ashley.e.shultz@gmail.com>
2017-08-15 16:06:02 -0500

    added updated navigation

--- rules/customnavigation.py
+++ rules/customnavigation.py
@@ -2,28 +2,32 @@ from caster.lib import control
 from caster.lib.dfplus.merge.mergerule import MergeRule
 from caster.lib.dfplus.state.short import R
 
-from dragonfly import Text
+from caster.lib.dfplus.additions import IntegerRefST
+
+from dragonfly import Text, Repeat
 from dragonfly.actions.action_key import Key
 
 class CustomNavigation(MergeRule):
     pronunciation = "custom navigation"
     mapping = {
-        "pine chat":                                R(Key("w-1"), rdescript="Maximize Window"), 
-        "kanban":                                   R(Key("w-2"), rdescript="Maximize Window"), 
-        "chromium":                                 R(Key("w-4"), rdescript="Maximize Window"), 
-        "slime text":                               R(Key("w-5"), rdescript="Maximize Window"), 
-        "file system":                              R(Key("w-6"), rdescript="Maximize Window"), 
-        "(get bash) | git bash":                    R(Key("w-8"), rdescript="Maximize Window"), 
-        "(data grip) | (datagram) | (data group)":  R(Key("w-7"), rdescript="Maximize Window"), 
+        "pine chat":                                R(Key("w-1"), rdescript="Custom Navigation: Alpine Chat"), 
+        "kanban":                                   R(Key("w-2"), rdescript="Custom Navigation: Kanban"), 
+        "get help | git hub":                       R(Key("w-3"), rdescript="Custom Navigation: GitHub"), 
+        "chromium":                                 R(Key("w-4"), rdescript="Custom Navigation: Chrome"), 
+        "slime tech":                               R(Key("w-5"), rdescript="Custom Navigation: Sublime Text"), 
+        "file system":                              R(Key("w-6"), rdescript="Custom Navigation: Open File Browser"), 
+        "sequel server":                            R(Key("w-7"), rdescript="Custom Navigation: MSSQL Management Studio"), 
+        "(get bash) | git bash":                    R(Key("w-8"), rdescript="Custom Navigation: ConEmu"), 
         
-        "open new file":                            R(Key("c-n"), rdescript="Maximize Window"), 
-        "homer":                                    R(Key("home"), rdescript="Maximize Window"), 
-        "ender":                                    R(Key("end"), rdescript="Maximize Window"), 
-        "open new file":                            R(Key("c-n"), rdescript="Maximize Window"),
+        "open new file":                            R(Key("c-n"), rdescript="Custom Navigation: Open New File"), 
+        "homer":                                    R(Key("home"), rdescript="Custom Navigation: Home Key"), 
+        "ender":                                    R(Key("end"), rdescript="Custom Navigation: End Key"), 
+        "toggle tab":                               R(Key("c-tab"), rdescript="Next Tab"),
+        "back tab [<n>]":                           R(Key("cs-tab"), rdescript="Previous Tab") * Repeat(extra="n"),
         }
     extras = [
+        IntegerRefST("n", 1, 10),
     ]
-    defaults = {
-    }
+    defaults = {"n":1}
 
 control.nexus().merger.add_global_rule(CustomNavigation())
