4cfa1fcd76f3c3967a3cf37ae3a2961f7a3623c7
AGiantSquid <ashley@greenkeytech.com>
2018-02-09 23:03:36 -0600

    adding voiceCode like commands

--- rules/customnavigation.py
+++ rules/customnavigation.py
@@ -4,21 +4,22 @@ from caster.lib.dfplus.state.short import R
 
 from caster.lib.dfplus.additions import IntegerRefST
 
-from dragonfly import Text, Repeat
+from dragonfly import Repeat
 from dragonfly.actions.action_key import Key
+
 
 class CustomNavigation(MergeRule):
     pronunciation = "custom navigation"
-    mapping = {     
-        "open new file":                            R(Key("c-n"), rdescript="Custom Navigation: Open New File"), 
-        "homer":                                    R(Key("home"), rdescript="Custom Navigation: Home Key"), 
-        "ender | under":                            R(Key("end"), rdescript="Custom Navigation: End Key"), 
+    mapping = {
+        "open new file":                            R(Key("c-n"), rdescript="Custom Navigation: Open New File"),
+        "Lefty":                                    R(Key("home"), rdescript="Custom Navigation: Home Key"),
+        "Ricky":                                    R(Key("end"), rdescript="Custom Navigation: End Key"),
         "toggle tab":                               R(Key("c-tab"), rdescript="Next Tab"),
         "back tab [<n>]":                           R(Key("s-tab"), rdescript="Previous Tab") * Repeat(extra="n"),
-        }
+    }
     extras = [
         IntegerRefST("n", 1, 10),
     ]
-    defaults = {"n":1}
+    defaults = {"n": 1}
 
 control.nexus().merger.add_global_rule(CustomNavigation())
--- rules/custompunctuation.py
+++ rules/custompunctuation.py
@@ -10,11 +10,11 @@ from caster.lib.dfplus.state.short import R
 
 class CustomPunctuation(MergeRule):
     pronunciation = "custom punctuation"
-    mapping = {  
+    mapping = {
 
-        #my custom overrides
+        # my custom overrides
         "double quotes":                            R(Key("dquote,dquote,left"), rdescript="Quotation Marks"),
-        "quotes":                                   R(Key("apostrophe,apostrophe,left"), rdescript="Thin Quotation Marks"),
+        "Quach it":                                 R(Key("apostrophe,apostrophe,left"), rdescript="Thin Quotation Marks"),
         "equals | equal to":                        R(Text(" = "), rdescript="ColdFusion: Equals"),
         "not equals | not equal to":                R(Text(" != "), rdescript="ColdFusion: Not Equal To"),
         "is equal to":                              R(Text(" == "), rdescript="ColdFusion: Not Equal To"),
@@ -22,44 +22,45 @@ class CustomPunctuation(MergeRule):
         "[is] less than":                           R(Text(" < "), rdescript="< Comparison"),
         "[is] greater [than] [or] equal [to]":      R(Text(" >= "), rdescript=">= Comparison"),
         "[is] less [than] [or] equal [to]":         R(Text(" <= "), rdescript="<= Comparison"),
-        "left curly":                               R(Key("lbrace"), rdescript="Left Curly Brace"),
-        "right curly":                              R(Key("rbrace"), rdescript="Right Curly Brace"),
+        "kirksorp":                                 R(Key("lbrace"), rdescript="Left Curly Brace"),
+        "kirkos":                                   R(Key("rbrace"), rdescript="Right Curly Brace"),
         "plus":                                     R(Text(" + "), rdescript="ColdFusion: Plus"),
         "minus":                                    R(Text(" - "), rdescript="ColdFusion: Minus"),
 
-        #this is same as the default punctuation
-        "semper":                                   R(Key("semicolon"), rdescript="Semicolon"),              
+        # this is same as the default punctuation
+        "sinker":                                   R(Key("semicolon"), rdescript="Semicolon"),
         "prekris":                                  R(Key("lparen, rparen, left"), rdescript="Parentheses"),
         "brax":                                     R(Key("lbracket, rbracket, left"), rdescript="Square Brackets"),
         "curly":                                    R(Key("lbrace, rbrace, left"), rdescript="Curly Braces"),
         "angle":                                    R(Key("langle, rangle, left"), rdescript="Angle Brackets"),
         "(pipe | pipes) (sim | symbol)":            R(Text("|"), rdescript="Pipe Symbol"),
         "pipes and":                                R(Text("|"), rdescript="Pipe Symbol"),
-        'ace [<npunc>]':                            R(Key("space"), rdescript="Space") * Repeat(extra="npunc"),
+        'skoosh [<npunc>]':                         R(Key("space"), rdescript="Space") * Repeat(extra="npunc"),
         "clamor":                                   R(Text("!"), rdescript="Exclamation Mark"),
         "deckle":                                   R(Text(":"), rdescript="Colon"),
         "starling":                                 R(Key("asterisk"), rdescript="Asterisk"),
         "questo":                                   R(Text("?"), rdescript="Question Mark"),
         "comma":                                    R(Text(","), rdescript="Comma"),
-        "carrot":                                   R(Text("^"), rdescript="Carat"),                     
+        "carrot":                                   R(Text("^"), rdescript="Carat"),
         "(period | dot)":                           R(Text("."), rdescript="Dot"),
         "at sign":                                  R(Text("@"), rdescript="At Sign"),
         "hash tag | pound sign":                    R(Text("#"), rdescript="Hash Tag"),
         "apostrophe":                               R(Text("'"), rdescript="Apostrophe"),
-        "underscore":                               R(Text("_"), rdescript="Underscore"),        
+        "underscore":                               R(Text("_"), rdescript="Underscore"),
         "backslash":                                R(Text("\\"), rdescript="Back Slash"),
         "slash":                                    R(Text("/"), rdescript="Forward Slash"),
         "Dolly":                                    R(Text("$"), rdescript="Dollar Sign"),
-        "modulo":                                   R(Key("percent"), rdescript="Percent Sign"),
-        'tabby [<npunc>]':                          R(Key("tab"), rdescript="Tab") * Repeat(extra="npunc"),
-        "boom":                                     R(Text(", "), rdescript="Comma + Space"),
+        "Percy":                                   R(Key("percent"), rdescript="Percent Sign"),
+        'tarp [<npunc>]':                          R(Key("tab"), rdescript="Tab") * Repeat(extra="npunc"),
+        'tarsh [<npunc>]':                          R(Key("tab"), rdescript="Tab") * Repeat(extra="npunc"),
+        "swipe":                                     R(Text(", "), rdescript="Comma + Space"),
     }
 
     extras = [
-            IntegerRefST("npunc", 0, 10),
+        IntegerRefST("npunc", 0, 10),
     ]
     defaults = {
-            "npunc": 1,
+        "npunc": 1,
     }
 
 control.nexus().merger.add_global_rule(CustomPunctuation())
