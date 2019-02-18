c3478e8a513f6e1dd8028079bbc15b576d1e62e9
AGiantSquid <ashley@greenkeytech.com>
2019-02-18 01:11:13 -0600

    add custom commands to user file

--- rules/__init__.py
+++ rules/__init__.py
@@ -1,12 +1,25 @@
 import glob
+import inspect
 import os
-modules = glob.glob(os.path.dirname(__file__) + "/*.py")
-__all__ = [os.path.basename(f)[:-3]
-           for f in modules if not f.endswith('__init__.py')]
 
-from customvocabulary import CustomVocabulary
-from custompunctuation import CustomPunctuation
-from uppercasesql import UpperCaseSQL
-from customnavigation import CustomNavigation
-from navigationbar.customnavigationbar import CustomNavigationBar
-from yarn import YarnCommands
+
+def is_valid(module):
+    ''' This function attempts to import the rules in order to detect 
+    errors in their implementation . After they are imported, they are garbage collected
+    when the function returns.'''
+    try:
+        _ = __import__(module, globals(), locals())
+        return True
+    except Exception as e:
+        print("Ignoring application '{}'. Failed to load with: ".format(module))
+        print(e)
+        return False
+
+
+modules = glob.glob(os.path.dirname(__file__) + "/*.py")
+# only valid rules will be added to the list
+__all__ = [
+    os.path.basename(f)[:-3]
+    for f in modules
+    if (not f.endswith('__init__.py') and is_valid(os.path.basename(f)[:-3]))
+]
